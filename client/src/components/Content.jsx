f17649eb1003e54925917cce9b36eccb4c3c1907
TOPE <topemicheal@yahoo.com>
2022-05-18 15:32:49 -0400

    CHORE: task 2 completed - spaghetti<>

--- client/src/components/Content.jsx
+++ client/src/components/Content.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './contentScreen.css';
 import log from '../lib/script';
 import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+
 
 log("hello world")
 
@@ -9,11 +12,54 @@ log("hello world")
 
 export default function Content() {
 
+/* FIRST TASK 
+* @DESC
+*   Make it so that you have a counter thats displayed in your content.
+*   Every time you click the button, the counter will increment by 1
+*   The content should reflect the new counter value.
+*/
+
+/* SECOND TASK 
+* @DESC
+*   When the component starts up, have it make an ajax call to https://online.carshop.com/tenant-data/tenant.json
+*   Set the title of the page to the "Title" of that api
+*/
+
+
   // useState for managing data / state (data) management.
   // called a hook in react called useState inside function component
   // useState declare a state variable to call the clicks variable
 
   const [clicks , incrementCounter] = useState(10) // 0 as initial state. Stored 0 in state
+  //const [data , apiCall ] = useState(10) 
+  
+  const URL = "https://online.carshop.com/tenant-data/tenant.json";
+
+  const params = {
+      method: 'GET',
+      url: URL
+  };
+  
+  useEffect(() => {
+    
+    const returnedData = async () => {
+        try {
+            const response = await axios(params);
+            const { title } = response.data;
+            log(title);
+            return title;
+
+        } catch (error) {
+            if ( error instanceof ReferenceError ) log('reference error');
+            if ( error instanceof TypeError ) log('type error');
+            else console.error(error);
+        }
+    }
+
+    returnedData().then(resp => document.title = resp);
+  });
+
+
 
   const increment = () => { incrementCounter(clicks + 1) }
   const decrement = () => { incrementCounter(clicks - 1) }
